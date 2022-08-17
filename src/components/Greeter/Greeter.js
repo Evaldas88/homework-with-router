e9e431bca56ec4c9ec87c6d05311e9e7c839b03f
Evaldas88 <e.skackauskas@gmail.com>
2022-08-17 16:08:24 +0300

    Created Greeter and Added boostrap

--- src/components/Greeter/Greeter.js
+++ src/components/Greeter/Greeter.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Greeter.css';
+import React, { useState } from "react";
+import Button1 from "../Button1/Button1";
+import "./Greeter.css";
 
-const Greeter = () => (
-  <div className="Greeter" data-testid="Greeter">
-    Greeter Component
-  </div>
-);
+const Greeter = () => {
+  const [name, setName] = useState({ value: "", wasCleared: false });
+  const [showGreeting, setShowGreeting] = useState(false);
 
-Greeter.propTypes = {};
+  const handleTextDisplay = () =>
+    name.value !== "" && name.wasCleared === false
+      ? setShowGreeting(!showGreeting)
+      : name.value === "" && name.wasCleared === true && showGreeting
+      ? (setShowGreeting(!showGreeting), (name.wasCleared = false))
+      : null;
 
-Greeter.defaultProps = {};
+  return (
+    <>
+      <div className="  text-center ">
+        <div className="d-flex justify-content-md-center mt-3">
+          <input
+            className="input1"
+            placeholder="What's your name?"
+            onKeyUp={(e) => {
+              setName({
+                value: e.target.value,
+                wasCleared: e.target.value === "" && e.key === "Backspace",
+              });
+            }}
+          />
+        </div>
+        <div className="mt-5 ">
+          <Button1
+          
+            color={
+              showGreeting ? (name.value.length > 0 ? `red` : `green`) : `blue`
+            }
+            text={showGreeting ? `Hide` : `Show`}
+            parentCallback={handleTextDisplay}
+          />
+
+          <label className="ps-3">
+            {showGreeting ? `Hi  ${name.value}` : ``}
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Greeter;
