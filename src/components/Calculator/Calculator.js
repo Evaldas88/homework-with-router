8b2ffb41b41ab95c5477683eaab86dc6070150c7
Evaldas88 <e.skackauskas@gmail.com>
2022-08-17 16:09:13 +0300

    Added buttons

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import './Calculator.css';
+import Button from '../Button/Button';
 
-const Calculator = () => (
-  <div className="Calculator" data-testid="Calculator">
-    Calculator Component
-  </div>
-);
+const Calculator = () => {
+  let [number, setNumber] = useState("0")
+  let [number1, setNumber1] = useState("0")
+  let [sum, setSum] = useState("0")
 
-Calculator.propTypes = {};
+  return (
+    <div className="container  text-center ">
 
-Calculator.defaultProps = {};
+      <div className="d-flex justify-content-md-center mt-3">
+        <input className="m-2" type="text" onChange={(e) => setNumber(e.target.value)}></input>
+        <input className="m-2" type="text" onChange={(e) => setNumber1(e.target.value)}></input>
+      </div>
+      <div className="mt-5">
+        <div className="d-flex justify-content-md-center m-3">
+          <Button
 
+            first={number}
+            second={number1}
+            text={"+"}
+            operator={"+"}
+            setSum={setSum}
+          />
+          <Button
+            first={number}
+            second={number1}
+            text={"-"}
+            operator={"-"}
+            setSum={setSum}
+          />
+        </div>
+        <div className="d-flex justify-content-md-center m-3">
+          <Button
+            first={number}  
+            second={number1}
+            text={"*"}
+            operator={"*"}
+            setSum={setSum}
+          />
+          <Button
+            first={number}
+            second={number1}
+            text={"/"}
+            operator={"/"}
+            setSum={setSum}
+          />
+        </div>
+        <p className="m-4 fs-3 "> Result: {sum} </p>
+      </div>
+    </div>)
+}
+    
 export default Calculator;
